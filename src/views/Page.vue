b1fee5fc6119f86592032cb43c20f90e3b8105a2
zhangye <zhangye01@changba.com>
2021-02-16 19:08:48 +0800

    add-router

---
+++ src/views/Page.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>page{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: 1
+    }
+  },
+  created() {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData () {
+      this.id = this.$route.query.id
+    }
+  }
+}
+</script>
